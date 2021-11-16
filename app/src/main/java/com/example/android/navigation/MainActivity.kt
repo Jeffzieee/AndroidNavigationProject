01459ef4f5ae1661ca363fbfdde95cfb4b6b2fa2
Jephin John <jephinjohn85@gmail.com>
2021-11-16 11:18:42 +0530

    COMMITED -->Navigation Drawer RESTRICTION added.

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -20,6 +20,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
@@ -30,15 +32,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
-        val binding  = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
         //Finding the Navigation Controller and linking it with the Action Bar
         val navController = this.findNavController(R.id.customNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
-
+        //To ensure NavigationDrawer doesn't work other than startDestination
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
     }
-
     // Function of the Back button to NavigateUp
     override fun onSupportNavigateUp() : Boolean = NavigationUI.navigateUp(this.findNavController(R.id.customNavHostFragment), drawerLayout)
 }
